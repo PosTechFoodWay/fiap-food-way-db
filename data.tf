4c3a19097ac41f584de965249db075732f6dd836
Joao Gabriel Mauricio <joao.mauricio@hatchworks.com>
2024-09-28 14:18:24 -0300

    Fix typo in VPC and Subnet tag values

--- data.tf
+++ data.tf
@@ -1,28 +1,28 @@
 data "aws_vpc" "base_vpc" {
   filter {
-    name   = "tag:Name"
-    values = ["Base VPC"]
+    name = "tag:Name"
+    values = ["BaseVPC"]
   }
 }
 
 data "aws_subnet" "private_subnet_1" {
   filter {
-    name   = "vpc_id"
+    name = "vpc_id"
     values = [data.aws_vpc.base_vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = ["PrivateSubnet1"]
   }
 }
 
 data "aws_subnet" "private_subnet_2" {
   filter {
-    name   = "vpc_id"
+    name = "vpc_id"
     values = [data.aws_vpc.base_vpc.id]
   }
   filter {
-    name   = "tag:Name"
+    name = "tag:Name"
     values = ["PrivateSubnet2"]
   }
 }

cbe862c26017ae0f7b10b1bb35256cf4d3585d5b
Joao Gabriel Mauricio <joao.mauricio@hatchworks.com>
2024-09-28 14:14:37 -0300

    Add AWS VPC data source and update subnet filters
    Moved the AWS VPC data source definition to the top of data.tf and updated the subnet filters to depend on the VPC ID. Also, added .idea directory to .gitignore for IDE-specific files.

--- data.tf
+++ data.tf
@@ -1,4 +1,15 @@
+data "aws_vpc" "base_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["Base VPC"]
+  }
+}
+
 data "aws_subnet" "private_subnet_1" {
+  filter {
+    name   = "vpc_id"
+    values = [data.aws_vpc.base_vpc.id]
+  }
   filter {
     name   = "tag:Name"
     values = ["PrivateSubnet1"]
@@ -7,14 +18,11 @@ data "aws_subnet" "private_subnet_1" {
 
 data "aws_subnet" "private_subnet_2" {
   filter {
-    name   = "tag:Name"
-    values = ["PrivateSubnet2"]
+    name   = "vpc_id"
+    values = [data.aws_vpc.base_vpc.id]
   }
-}
-
-data "aws_vpc" "base_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["Base VPC"]
+    values = ["PrivateSubnet2"]
   }
 }
